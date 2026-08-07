517b5690f7a13f519a905bf0266ce13e3ab31921
joshua <joshuajgomez@gmail.com>
2026-08-07 17:23:41 +0200

    Fix compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.joshgm3z.netplayer"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
