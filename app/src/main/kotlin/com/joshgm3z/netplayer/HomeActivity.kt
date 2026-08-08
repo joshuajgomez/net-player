c2743ec354d513a06c4f61bc39de303309b9de6d
joshua <joshuajgomez@gmail.com>
2026-08-08 23:34:34 +0200

    Darker background

--- app/src/main/kotlin/com/joshgm3z/netplayer/HomeActivity.kt
+++ app/src/main/kotlin/com/joshgm3z/netplayer/HomeActivity.kt
@@ -4,18 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.tv.material3.MaterialTheme.colorScheme
 import com.joshgm3z.netplayer.ui.TvNavHost
-import com.joshgm3z.netplayer.ui.screen.HomeScreen
-import com.joshgm3z.netplayer.ui.screen.PlayerScreen
 import com.joshgm3z.netplayer.ui.theme.NetPlayerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
--- app/src/main/kotlin/com/joshgm3z/netplayer/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/joshgm3z/netplayer/ui/theme/Theme.kt
@@ -1,13 +1,15 @@
 package com.joshgm3z.netplayer.ui.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.darkColorScheme
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = Color.Black
 )
 
 @Composable
