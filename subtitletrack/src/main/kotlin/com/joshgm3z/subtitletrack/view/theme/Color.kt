51bff26c3e62de8f2266e6a2cd3b4c84ab60a5f3
joshua <joshuajgomez@gmail.com>
2026-08-11 06:54:29 +0200

    Some change5

--- subtitletrack/src/main/kotlin/com/joshgm3z/subtitletrack/view/theme/Color.kt
+++ subtitletrack/src/main/kotlin/com/joshgm3z/subtitletrack/view/theme/Color.kt
@@ -12,7 +12,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val Gray10 = Color(0xFF171717)
+val Gray10 = Color(0xFF101010)
 
 @Composable
 fun textColor() = colorScheme.onSurface
